8ac4d5656fed43fee679fb0cee41eeadcb87fd46
1dominik7 <dominik-wicek@o2.pl>
2025-09-13 20:43:44 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   client/package-lock.json
    	modified:   client/package.json
    	modified:   client/src/components/homePage/BannerHome.tsx
    	modified:   client/tsconfig.json

--- client/src/components/homePage/BannerHome.tsx
+++ client/src/components/homePage/BannerHome.tsx
@@ -1,12 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Swiper as SwiperType } from "swiper";
+import { Swiper as SwiperType, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
-import SwiperCore from "swiper";
-import { Autoplay } from "swiper";
 import LoadingAnimation from "../../ui/LoadingAnimation";
 import { api } from "../../config/api";
 import { useQuery } from "@tanstack/react-query";
@@ -19,7 +17,6 @@ interface Image {
 }
 
 const BannerHome = () => {
-  SwiperCore.use([Autoplay]);
   const swiperRef = useRef<SwiperType | null>(null);
 
   const [progress, setProgress] = useState(0);
